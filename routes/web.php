9c9a1d11af5477e3c00c1e122ea5f3dfc1f41862
Andrés Pinto <aapcx55@gmail.com>
2020-11-18 13:33:25 -0500

    ...

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 
-use App\Http\Controllers\ExpenseController;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\LivewireController;
+use App\Http\Controllers\ExpenseController;
 
 /*
 |--------------------------------------------------------------------------
